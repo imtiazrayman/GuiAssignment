407ee275912e6432853b2bc790d024adea2b7ad6
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-12 20:56:37 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace GuiAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.CopyBtn = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.WriteBtn = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileOpeningToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileWriteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpScreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Entered File Name : ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(354, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Count";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CopyBtn
+            // 
+            this.CopyBtn.Location = new System.Drawing.Point(354, 162);
+            this.CopyBtn.Name = "CopyBtn";
+            this.CopyBtn.Size = new System.Drawing.Size(75, 23);
+            this.CopyBtn.TabIndex = 9;
+            this.CopyBtn.Text = "Copy->";
+            this.CopyBtn.UseVisualStyleBackColor = true;
+            this.CopyBtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(397, 111);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 10;
+            this.btn.Text = "Show";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(316, 111);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Hide";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(285, 340);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(485, 87);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(280, 340);
+            this.richTextBox2.TabIndex = 13;
+            this.richTextBox2.Text = "";
+            // 
+            // WriteBtn
+            // 
+            this.WriteBtn.Location = new System.Drawing.Point(354, 328);
+            this.WriteBtn.Name = "WriteBtn";
+            this.WriteBtn.Size = new System.Drawing.Size(75, 23);
+            this.WriteBtn.TabIndex = 14;
+            this.WriteBtn.Text = "Write";
+            this.WriteBtn.UseVisualStyleBackColor = true;
+            this.WriteBtn.Click += new System.EventHandler(this.WriteBtn_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(354, 272);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(354, 387);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Exit";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(301, 49);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(145, 20);
+            this.txtSearch.TabIndex = 17;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(452, 46);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 18;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.fileWriteToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 29);
+            this.menuStrip1.TabIndex = 19;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileOpeningToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(46, 25);
+            this.openToolStripMenuItem.Text = "File";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // fileOpeningToolStripMenuItem
+            // 
+            this.fileOpeningToolStripMenuItem.Name = "fileOpeningToolStripMenuItem";
+            this.fileOpeningToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.fileOpeningToolStripMenuItem.Text = "File Open";
+            this.fileOpeningToolStripMenuItem.Click += new System.EventHandler(this.fileOpeningToolStripMenuItem_Click);
+            // 
+            // fileWriteToolStripMenuItem
+            // 
+            this.fileWriteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToFileToolStripMenuItem});
+            this.fileWriteToolStripMenuItem.Name = "fileWriteToolStripMenuItem";
+            this.fileWriteToolStripMenuItem.Size = new System.Drawing.Size(107, 25);
+            this.fileWriteToolStripMenuItem.Text = "Write To File";
+            // 
+            // saveToFileToolStripMenuItem
+            // 
+            this.saveToFileToolStripMenuItem.Name = "saveToFileToolStripMenuItem";
+            this.saveToFileToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.saveToFileToolStripMenuItem.Text = "Save to File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpScreenToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(54, 25);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // helpScreenToolStripMenuItem
+            // 
+            this.helpScreenToolStripMenuItem.Name = "helpScreenToolStripMenuItem";
+            this.helpScreenToolStripMenuItem.Size = new System.Drawing.Size(163, 26);
+            this.helpScreenToolStripMenuItem.Text = "Help Screen";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(46, 25);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(544, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Replace";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.WriteBtn);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.CopyBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Text Reader/ Writer ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button CopyBtn;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button WriteBtn;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileOpeningToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileWriteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpScreenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GuiAssignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName = Microsoft.VisualBasic.Interaction.InputBox("Please enter filename :", "File Name", "", 60, 90);
+                label1.Text = label1.Text + fileName;
+
+            if (fileName.isEmpty())
+            {
+
+            }
+                StreamReader myFile;
+
+            try
+            {
+                myFile = File.OpenText(fileName);
+                string line = "";
+                do
+                {
+                    line = myFile.ReadLine();
+                    if (line != null)
+                    {
+                        richTextBox1.AppendText(Environment.NewLine + line);
+                    }
+                } while (line != null);
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("File Not Found", "File Location Error",
+               MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string countText;
+            countText = Convert.ToString(richTextBox1.Text.Count());
+            MessageBox.Show("The Character Count is:" +
+           countText, "Characters", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+            richTextBox1.Copy();
+            richTextBox2.Paste();
+        }
+
+        private void WriteBtn_Click(object sender, EventArgs e)
+        {
+            StreamWriter myFile;
+            string fileName = Microsoft.VisualBasic.Interaction.InputBox("Please Name Your File:", "Enter File Name", "", 60, 90);
+            myFile = File.CreateText(fileName);
+            try
+            {
+                for (int i = 0; i < richTextBox2.Lines.Length; i++)
+                {
+                    myFile.WriteLine(richTextBox2.Lines[i]);
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Error Writing to File", "File Write Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            myFile.Close();
+            MessageBox.Show("File Creation " + fileName + " was successful!", "File Creation Success", MessageBoxButtons.OK,
+           MessageBoxIcon.Information);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            int start = 0;
+            int end = richTextBox2.Text.LastIndexOf(txtSearch.Text);
+            while (start < end)
+            {
+                richTextBox2.Find(txtSearch.Text, start, richTextBox2.TextLength, RichTextBoxFinds.MatchCase);
+                richTextBox2.SelectionBackColor = Color.GreenYellow;
+                start = richTextBox2.Text.IndexOf(txtSearch.Text, start) + 1;
+
+            }
+
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileOpeningToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string fileName =
+      Microsoft.VisualBasic.Interaction.InputBox("Please enter filename :", "File Name", "", 60, 90);
+            label1.Text = label1.Text + fileName;
+            StreamReader myFile;
+            try
+            {
+                myFile = File.OpenText(fileName);
+                string line = "";
+                do
+                {
+                    line = myFile.ReadLine();
+                    if (line != null)
+                    {
+                        richTextBox1.AppendText(Environment.NewLine + line);
+                    }
+                } while (line != null);
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("File Not Found", "File Location Error",
+               MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
